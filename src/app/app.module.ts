6fd9433c6c7402dfc10bc58b7d2f99292634ddce
Nils Kreutzer <nkr@netempire.de>
2022-11-05 13:16:38 +0100

    content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { TranslateModule, TranslateLoader, TranslatePipe } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LocationComponent } from './location/location.component';
 import { FaqComponent } from './faq/faq.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -16,13 +24,21 @@ import { FaqComponent } from './faq/faq.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {path: '', component: HomepageComponent},
       {path: 'faq', component: FaqComponent},
       {path: 'location', component: LocationComponent},
     ]),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [TranslatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
